393fbc989b2f501bcd0a2fb0b4b886035074e94b
Octavio Calleya <octaviospain@gmail.com>
2017-03-22 13:24:39 +0100

    Added RSS parser library, Logger and maven usage. Added some lines to test the library

---
+++ run.sh
@@ -0,0 +1,1 @@
+mvn package && mvn exec:java -D exec.mainClass="cuni.software.CustomArticleFinderRunner" -Dexec.args="src/main/resources/rss-feeds.txt"
---
+++ src/main/java/cuni/software/CustomArticleFinderRunner.java
@@ -0,0 +1,37 @@
+package cuni.software;
+
+import org.slf4j.*;
+
+import java.io.*;
+import java.util.*;
+
+public class CustomArticleFinderRunner {
+
+    private static final Logger LOG = LoggerFactory.getLogger(CustomArticleFinderRunner.class.getName());
+
+    private final static Scanner input = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        System.out.println("****\tCustom Article Finder\t****");
+        String query = "";
+
+        try {
+            List<RssFeed> loadedRssFeeds = RssFeedParse.fromFile(new File(args[0]));
+            System.out.println("\n\tLoaded rss feeds:");
+            loadedRssFeeds.forEach(System.out::println);
+            SearchEngine searchEngine = new SearchEngine(loadedRssFeeds);
+
+            while (! query.equalsIgnoreCase("exit")) {
+                System.out.println("\n\tEnter the query in order to find related articles. Type 'exit' to quit.");
+                query = input.nextLine();
+
+                ArticleFinder articleFinder = new ArticleFinder(query, searchEngine);
+                articleFinder.findRelatedArticles().forEach(article -> System.out.println(article.getUri()));
+            }
+        }
+        catch (Exception exception) {
+            LOG.error("Error: " + exception.getMessage());
+            exception.printStackTrace();
+        }
+    }
+}
